242d53ca82bcde7941261ab61ee04d397fbb7233
Nazar Mosula <nmosula@gmail.com>
2023-01-08 11:45:39 +0200

    get countries json.data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./css/styles.css";
-import "./js/fetchCountries.js";
+import {fetchCountries} from "./js/fetchCountries.js";
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
 
 const inpSearch = document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
@@ -14,6 +14,30 @@ inpSearch.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 function onInputCountry() {
     const searchCountry = inpSearch.value.trim();
 
+    if (searchCountry == '') return;
+    else if(searchCountry !== '') {
     
-    console.log(fetchCountries(searchCountry));
+        fetchCountries(searchCountry)
+            .then(data => {
+                if (data.length == 1)
+                    showCountryInfo(data);
+                else if (data.length > 1 && data.length <= 10)
+                    showCountriesInfo(data);
+                else if (data.length > 10)
+                    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.", { timeout: 5000 });
+
+            })
+            .catch(err => console.log(err));
+    }
+}
+
+function showCountryInfo(data) {
+    console.log("Info One country");
+    console.log(data);
+}
+
+
+function showCountriesInfo(data) {
+    console.log("Info many countries");
+    console.log(data);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,16 @@
+import Notiflix from "notiflix";
+
 export function fetchCountries(name) {
-    return name;
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status === 404) {
+                    Notiflix.Notify.failure("Oops, there is no country with that name", { timeout: 5000 });
+                }
+                throw new Error(resp.statusText);
+            }
+
+            return resp.json();
+        })
 }
